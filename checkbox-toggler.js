055115f3c0fd05ee09ffac856a2c146ac92383dd
kcox5172 <kcox5172@gmail.com>
2014-08-13 11:16:56 -0500

    Update checkbox-toggler.js
    Wrapped in IIFE to pass in jQuery

--- checkbox-toggler.js
+++ checkbox-toggler.js
@@ -1,53 +1,57 @@
-$.fn.checkboxToggler = function(selector, callback) {
-	
-	/**
-	 *	$('parentCheckbox').checkboxToggler('[name="delete"]');
-	 *
-	 *	accepts optional callback
-	 *
-	 *	$('parentCheckbox').checkboxToggler('[name="delete"]', function(numChildren, numChecked) {
-	 *		console.log(numChildren, numChecked);	
-	 *	});
-	 */
+(function($) {
 
-	return this.each(function() {
+	$.fn.checkboxToggler = function(selector, callback) {
 		
-		var	$parent = $(this);
-		var $children = $(selector);
-		var numChildren = $children.length;
-		var numChecked = $children.filter(':checked').length;
-		
-		if (numChecked === numChildren) {
-			$parent.prop('checked', true);
-		}
-		
-		$parent.on('click', function() {
+		/**
+		 *	$('parentCheckbox').checkboxToggler('[name="delete"]');
+		 *
+		 *	accepts optional callback
+		 *
+		 *	$('parentCheckbox').checkboxToggler('[name="delete"]', function(numChildren, numChecked) {
+		 *		console.log(numChildren, numChecked);	
+		 *	});
+		 */
+	
+		return this.each(function() {
+
+			var	$parent = $(this);
+			var $children = $(selector);
+			var numChildren = $children.length;
+			var numChecked = $children.filter(':checked').length;
 			
-			var isChecked = $parent.prop('checked');
-			
-			numChecked = isChecked ? numChildren : 0;
-			
-			$children.prop('checked', isChecked);
-			
-			if (callback) {
-				callback.call(null, numChildren, numChecked);
+			if (numChecked === numChildren) {
+				$parent.prop('checked', true);
 			}
 			
-		});
+			$parent.on('click', function() {
+				
+				var isChecked = $parent.prop('checked');
+				
+				numChecked = isChecked ? numChildren : 0;
+				
+				$children.prop('checked', isChecked);
+				
+				if (callback) {
+					callback.call(null, numChildren, numChecked);
+				}
+				
+			});
+		
+			$children.on('click', function() {
+				
+				var $this = $(this);
 	
-		$children.on('click', function() {
-			
-			var $this = $(this);
-
-			$this.prop('checked') ? numChecked++ : numChecked--;
-			$parent.prop('checked', (numChecked === numChildren));
-			
-			if (callback) {
-				callback.call(null, numChildren, numChecked);
-			}
+				$this.prop('checked') ? numChecked++ : numChecked--;
+				$parent.prop('checked', (numChecked === numChildren));
+				
+				if (callback) {
+					callback.call(null, numChildren, numChecked);
+				}
+				
+			});
 			
 		});
 		
-	});
+	};
 	
-};
+})(jQuery);
